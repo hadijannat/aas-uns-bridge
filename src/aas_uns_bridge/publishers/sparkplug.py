68fce41c252109a95523bbfb34e03cde12914fb8
hadijannat <aeroshariati@gmail.com>
2026-01-27 18:18:05 +0100

    fix: use relative imports for state modules

--- src/aas_uns_bridge/publishers/sparkplug.py
+++ src/aas_uns_bridge/publishers/sparkplug.py
@@ -7,7 +7,8 @@ from typing import Any
 from aas_uns_bridge.config import SparkplugConfig
 from aas_uns_bridge.domain.models import ContextMetric
 from aas_uns_bridge.mqtt.client import MqttClient
-from aas_uns_bridge.state.alias_db import AliasDB
+
+from ..state.alias_db import AliasDB
 
 logger = logging.getLogger(__name__)
 
